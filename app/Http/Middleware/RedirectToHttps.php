63ac19a979a5b535ef0a157297b6b5c2b32873dc
Shigeo Nakamoto <shigechi1024@gmail.com>
2018-06-07 01:44:57 +0000

    test RedirectToHttps

--- app/Http/Middleware/RedirectToHttps.php
+++ app/Http/Middleware/RedirectToHttps.php
@@ -15,10 +15,8 @@ class RedirectToHttps
      */
     public function handle($request, Closure $next)
     {
-        // config('app.env')を検証する！
         $environment = \App::environment();
-        echo $environment;
-        if ( !$this->is_ssl()/* && config('app.env') === 'production'*/ )
+        if ( !$this->is_ssl() && $environment === 'production' )
         {
             //return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
             echo "not secure";
