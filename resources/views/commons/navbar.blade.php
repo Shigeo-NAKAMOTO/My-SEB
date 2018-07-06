cc017da9783470d560825c6508bafd9658e03243
Shigeo Nakamoto <shigechi1024@gmail.com>
2018-07-06 09:46:36 +0000

    use link_to_route on navbar

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -23,8 +23,8 @@
                             </ul>
                         </li>
                     @else
-                        <li><a href="{{ url('signup/', null, $is_production) }}">アカウント作成（無料）</a></li>
-                        <li><a href="{{ url('login/', null, $is_production) }}">ログイン</a></li>
+                        <li>{!! link_to_route('signup.get', 'アカウント作成（無料）') !!}</li>
+                        <li>{!! link_to_route('login.get', 'ログイン') !!}</li>
                     @endif
                 </ul>
             </div>
